816ebd4abe5d84179ed03d555f828d6b59e1908b
Nicolas Perrault <nipe1783@colorado.edu>
2024-06-24 20:34:41 -0600

    v1

--- src/planners/Planner.cu
+++ src/planners/Planner.cu
@@ -9,6 +9,8 @@
 #include <chrono>
 
 #define STATE_DIM 4 // x,y,theta,v
+#define INPUT_DIM 3 // a, steering angle, dt
+#define length 1.0
 
 void checkCudaError(cudaError_t err, const char* msg) {
     if (err != cudaSuccess) {
@@ -17,132 +19,63 @@ void checkCudaError(cudaError_t err, const char* msg) {
     }
 }
 
+__device__
+void propagateState(float* x0, float* controls, float* x1, int numDisc){
+    printf("Block: %d, Thread: %d, paent: %f, %f, %f, %f\n", blockIdx.x, threadIdx.x, x0[0], x0[1], x0[2], x0[3]);
+    float dt = controls[2] / numDisc;
+    x1[0] = x0[0];
+    x1[1] = x0[1];
+    x1[2] = x0[2];
+    x1[3] = x0[3];
 
-// Global: called from the CPU to the GPU
-__global__ void generateRandomSampleKernel(const float *parent, float *samples, float *controls, const int numSamples, const float length){
-    // sample: [x, y, theta, v]
-    int sampleIdx = threadIdx.x;
-
-    curandState state;
-    curand_init((unsigned long long)clock() + sampleIdx, 0, 0, &state);
-
-    // Generate random controls
-    float a = curand_uniform(&state);
-    float steeringAngle = curand_uniform(&state);
-    float dt = curand_uniform(&state);
-
-    controls[sampleIdx * 3] = a;
-    controls[sampleIdx * 3 + 1] = steeringAngle;
-    controls[sampleIdx * 3 + 2] = dt;
-
-    // Generate sample
-    samples[sampleIdx * 4] = parent[0] + parent[3] * cos(parent[2]) * dt;
-    samples[sampleIdx * 4 + 1] = parent[1] + parent[3] * sin(parent[2]) * dt;
-    samples[sampleIdx * 4 + 2] = parent[2] + parent[3]/length * tan(steeringAngle) * dt;
-    samples[sampleIdx * 4 + 3] = parent[3] + a * dt;
-}
-
-// Global: called from the CPU to the GPU
-__global__ void generateRandomSampleKernelV2(const float *parents, float *samples, float *controls, const int numSamples, const float length){
-    int sampleIdx = blockIdx.x * STATE_DIM + threadIdx.x;
-
-    // load sample onto shared memory
-    __shared__ float parent[STATE_DIM];
-    if(threadIdx.x < STATE_DIM){
-        parent[threadIdx.x] = parents[blockIdx.x * STATE_DIM + threadIdx.x];
+    for (int i = 0; i < numDisc; i++) {
+        x1[0] = x1[0] + x1[3] * cosf(x1[2]) * dt;
+        x1[1] = x1[1] + x1[3] * sinf(x1[2]) * dt;
+        x1[2] = x1[2] + x1[3] / length * tanf(controls[1]) * dt;
+        x1[3] = x1[3] + controls[0] * dt;
     }
-    __syncthreads();
-
-    // seed random number generator
-    curandState state;
-    curand_init((unsigned long long)clock() + sampleIdx, 0, 0, &state);
-
-    // Generate random controls
-    float a = curand_uniform(&state);
-    float steeringAngle = curand_uniform(&state);
-    float dt = curand_uniform(&state);
-    controls[sampleIdx * 3] = a;
-    controls[sampleIdx * 3 + 1] = steeringAngle;
-    controls[sampleIdx * 3 + 2] = dt;
-
-    // Generate sample
-    samples[sampleIdx * 4] = parent[0] + parent[3] * cos(parent[2]) * dt;
-    samples[sampleIdx * 4 + 1] = parent[1] + parent[3] * sin(parent[2]) * dt;
-    samples[sampleIdx * 4 + 2] = parent[2] + parent[3]/length * tan(steeringAngle) * dt;
-    samples[sampleIdx * 4 + 3] = parent[3] + a * dt;
-
-}
-
-// Host function to manage GPU memory and launch kernel.
-// One sample per thread.
-// Each sample generates trajectory.
-void Planner::generateRandomSamples(const float* parent, const int numSamples, float **samples, float **controls) {
-    
-    float *d_parent; // Parent state. Device memory
-    float *h_samples = new float[numSamples * 4]; // Samples. Host memory
-    float *d_samples; // Samples. Device memory
-    float *h_controls = new float[numSamples * 3]; // Controls. Host memory
-    float *d_controls; // Controls. Device memory
-
-    // Allocate memory on the device
-    int size_parent = 4 * sizeof(float);
-    int size_samples = numSamples * 4 * sizeof(float);
-    int size_controls = numSamples * 3 * sizeof(float);
-    checkCudaError(cudaMalloc((void **)&d_parent, size_parent), "Failed to allocate device memory for parent state");
-    checkCudaError(cudaMalloc((void **)&d_samples, size_samples), "Failed to allocate device memory for samples");
-    checkCudaError(cudaMalloc((void **)&d_controls, size_controls), "Failed to allocate device memory for controls");
-
-    // Copy data from host to device
-    checkCudaError(cudaMemcpy(d_parent, parent, size_parent, cudaMemcpyHostToDevice), "Failed to copy parent state to device");
-
-    // Sizing grid and block.
-    dim3 dimGrid(1, 1, 1); // example. 1 block
-    dim3 dimBlock(numSamples, 1, 1); // example. numSamples threads per block
-
-    // call kernel
-    generateRandomSampleKernelV2<<<dimGrid, dimBlock>>>(d_parent, d_samples, d_controls, numSamples, 1.0);
-
-    // Copy data from device to host
-    checkCudaError(cudaMemcpy(h_samples, d_samples, size_samples, cudaMemcpyDeviceToHost), "Failed to copy samples to host");
-    checkCudaError(cudaMemcpy(h_controls, d_controls, size_controls, cudaMemcpyDeviceToHost), "Failed to copy controls to host");
-
-    // Free memory on the device
-    checkCudaError(cudaFree(d_parent), "Failed to free device memory for parent state");
-    checkCudaError(cudaFree(d_samples), "Failed to free device memory for samples");
-
-    // Assign pointers to the outputs
-    *samples = h_samples;
-    *controls = h_controls;
-
 }
 
 // Global: called from the CPU to the GPU
-__global__ void generateRandomTreeKernel(const float *parents, float *tree, float *controls, const int numIterations, const float length){
+__global__ 
+void generateRandomTreeKernel(const float* parents, float* tree, float* controls, const int numIterations) {
     
-    // load sample onto shared memory
+    // Load sample onto shared memory
     __shared__ float parent[STATE_DIM];
-    if(threadIdx.x < STATE_DIM){
+    if (threadIdx.x < STATE_DIM) {
         parent[threadIdx.x] = parents[threadIdx.x];
     }
     __syncthreads();
     
-    int col = blockIdx.x*blockDim.x + threadIdx.x;
-    for (int outRow = 0; outRow < numIterations; outRow++){
-        int outIndex = outRow*STATE_DIM + (outRow * gridDim.x * blockDim.x + col) * STATE_DIM;
-        // seed random number generator
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    for (int outRow = 0; outRow < numIterations; outRow++) {
+        int outIndex = outRow * gridDim.x * blockDim.x * STATE_DIM + col * STATE_DIM;
+        printf("Block: %d, Thread: %d, outIndex: %d\n", blockIdx.x, threadIdx.x, outIndex);
+        int controlIndex = outRow * gridDim.x * blockDim.x * INPUT_DIM + col * INPUT_DIM;
+
+        // Seed random number generator
         curandState state;
         curand_init((unsigned long long)clock() + outIndex, 0, 0, &state);
+
         // Generate random controls
-        float a = curand_uniform(&state);
-        float steeringAngle = curand_uniform(&state);
-        float dt = curand_uniform(&state);
-        tree[outIndex] = parent[0] + parent[3] * cos(parent[2]) * dt;
-        tree[outIndex + 1] = parent[1] + parent[3] * sin(parent[2]) * dt;
-        tree[outIndex + 2] = parent[2] + parent[3]/length * tan(steeringAngle) * dt;
-        tree[outIndex + 3] = parent[3] + a * dt;
-        if(threadIdx.x < STATE_DIM){
-            printf("New Parent Idx: %d\n", outRow*STATE_DIM + threadIdx.x);
-            parent[threadIdx.x] = tree[outRow*STATE_DIM + threadIdx.x];
+        float a = curand_uniform(&state) * 5.0f - 2.5f;  // Scale to range [-2.5, 2.5]
+        float steeringAngle = curand_uniform(&state) * M_PI - M_PI / 2;  // Scale to range [-pi/2, pi/2]
+        float dt = curand_uniform(&state) * 0.3f;  // Scale to range [0, .3]
+
+        // Write to controls array
+        controls[controlIndex] = a;
+        controls[controlIndex + 1] = steeringAngle;
+        controls[controlIndex + 2] = dt;
+
+        // Write to tree array
+        float* x1 = &tree[outIndex];
+        propagateState(parent, &controls[controlIndex], x1, 20);
+
+        __syncthreads();
+
+        // Update parent state for the next iteration
+        if (threadIdx.x < STATE_DIM) {
+            parent[threadIdx.x] = tree[outIndex + threadIdx.x];
         }
         __syncthreads();
     }
@@ -150,85 +83,83 @@ __global__ void generateRandomTreeKernel(const float *parents, float *tree, floa
 
 void Planner::generateRandomTree(const float* parent, const int numSamples, float **samples, float **controls){
 
-    const int threadsPerBlock = 5;
-    const int blocksPerGrid = 1;
+    // initialize execution parameters
+    const int threadsPerBlock = 4;
+    const int blocksPerGrid = 2;
     const int rowsTree = 3;
     const int colsTree = STATE_DIM * threadsPerBlock * blocksPerGrid;
-
-    float hTree[rowsTree][colsTree];
+    const int colsControls = INPUT_DIM * threadsPerBlock * blocksPerGrid;
     int sizeTree = rowsTree * colsTree * sizeof(float);
+    int sizeControls = rowsTree * colsControls * sizeof(float);
+    int sizeParent = STATE_DIM * sizeof(float);
+    dim3 dimGrid(blocksPerGrid, 1, 1);
+    dim3 dimBlock(threadsPerBlock, 1, 1);
+    float milliseconds = 0;
 
+    // host variables
+    float hTree[rowsTree][colsTree];
+    float hControls[rowsTree][colsControls];
+
+    // device variables
     float *dTree;
-    checkCudaError(cudaMalloc((void **)&dTree, sizeTree), "Failed to allocate device memory for tree");
-
+    float *dControls;
     float *dParent;
-    checkCudaError(cudaMalloc((void **)&dParent, STATE_DIM * sizeof(float)), "Failed to allocate device memory for parent state");
-    checkCudaError(cudaMemcpy(dParent, parent, STATE_DIM * sizeof(float), cudaMemcpyHostToDevice), "Failed to copy parent state to device");
 
-     // Create CUDA events
+    // allocate memory on device
+    checkCudaError(cudaMalloc((void **)&dTree, sizeTree), "Failed to allocate device memory for tree");
+    checkCudaError(cudaMalloc((void **)&dControls, sizeControls), "Failed to allocate device memory for controls");
+    checkCudaError(cudaMalloc((void **)&dParent, sizeParent), "Failed to allocate device memory for parent state");
+    
+    // copy parent state to device
+    checkCudaError(cudaMemcpy(dParent, parent, sizeParent, cudaMemcpyHostToDevice), "Failed to copy parent state to device");
+    
+    // Initialize Timer
     cudaEvent_t start, stop;
     checkCudaError(cudaEventCreate(&start), "Failed to create start event");
     checkCudaError(cudaEventCreate(&stop), "Failed to create stop event");
-
-    // Record the start event
     checkCudaError(cudaEventRecord(start), "Failed to record start event");
 
-    // Set kernel parameters and call kernel
-    dim3 dimGrid(blocksPerGrid, 1, 1);
-    dim3 dimBlock(threadsPerBlock, 1, 1);
-    generateRandomTreeKernel<<<dimGrid, dimBlock>>>(dParent, dTree, *controls, rowsTree, 1.0);
+    // Call the kernel
+    generateRandomTreeKernel<<<dimGrid, dimBlock>>>(dParent, dTree, dControls, rowsTree);
 
-    // Record the stop event
+    // Stop Timer
     checkCudaError(cudaEventRecord(stop), "Failed to record stop event");
-
-    // Wait for the stop event to complete
     checkCudaError(cudaEventSynchronize(stop), "Failed to synchronize stop event");
-
-    // Calculate the elapsed time
-    float milliseconds = 0;
     checkCudaError(cudaEventElapsedTime(&milliseconds, start, stop), "Failed to calculate elapsed time");
 
-    // Copy data from device to host
+    // Transfer data from device to host
     checkCudaError(cudaMemcpy(hTree, dTree, sizeTree, cudaMemcpyDeviceToHost), "Failed to copy tree to host");
+    checkCudaError(cudaMemcpy(hControls, dControls, sizeControls, cudaMemcpyDeviceToHost), "Failed to copy controls to host");
 
     // Free device memory
     checkCudaError(cudaFree(dTree), "Failed to free device memory for tree");
+    checkCudaError(cudaFree(dControls), "Failed to free device memory for controls");
+    checkCudaError(cudaFree(dParent), "Failed to free device memory for parent state");
 
     // Destroy CUDA events
     checkCudaError(cudaEventDestroy(start), "Failed to destroy start event");
     checkCudaError(cudaEventDestroy(stop), "Failed to destroy stop event");
 
-    // Print the elapsed time
+    // Print Information
     printf("Kernel execution time: %f milliseconds\n", milliseconds);
-
-    // print tree size:
     printf("Tree size: %d\n", rowsTree*colsTree);
 
-    // print tree:
-    // for (int i = 0; i < rowsTree; ++i) {
-    //     for (int j = 0; j < colsTree; ++j) {
-    //         printf("%f ", hTree[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-
-    // timing CPU version:
-    auto cpu_start = std::chrono::high_resolution_clock::now();
-
-    for (int i = 0; i < rowsTree; ++i) {
-        for (int j = 0; j < colsTree; ++j) {
-            hTree[i][j] = 0.0f; // Fill with zeros for example
+    // Save data to CSV
+    FILE *fp = fopen("samples.csv", "w");
+    for (int i = 0; i < rowsTree; i++){
+        for (int j = 0; j < colsTree; j++){
+            fprintf(fp, "%f,", hTree[i][j]);
         }
+        fprintf(fp, "\n");
     }
+    fclose(fp);
 
-    auto cpu_end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<float, std::milli> cpu_duration = cpu_end - cpu_start;
-
-    // Print CPU execution time
-    printf("CPU execution time: %f milliseconds\n", cpu_duration.count());
-
-
-
-
-
+    FILE *fp2 = fopen("controls.csv", "w");
+    for (int i = 0; i < rowsTree; i++){
+        for (int j = 0; j < colsControls; j++){
+            fprintf(fp2, "%f,", hControls[i][j]);
+        }
+        fprintf(fp2, "\n");
+    }
+    fclose(fp2);
 }
